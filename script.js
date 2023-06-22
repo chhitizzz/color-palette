cc47d512f9882afbae6ad961deb6977205d82259
ChhitizV <chhitizvaidya10@gmail.com>
2023-06-22 12:15:31 +0545

    li

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const container = document.querySelector(".container");
 const refreshBtn = document.querySelector(".refresh-button");
 
-const maxPaletteBoxes = 32;
+const maxPaletteBoxes = 10;
 
 const generatePalette = () => {
+    color.innerHTML = "";
     for(let i = 0; i < maxPaletteBoxes ; i++){
-            let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
-            randomHex = `#${randomHex.padStart(6,"0")}`;
-            console.log(randomHex);
+        let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+        randomHex = `#${randomHex.padStart(6,"0")}`;
+        console.log(randomHex);
 
-            const color = doccument.createElement("li");
-            color.classList.add("color");
-            color.innerHTML = `<div class="react-box" style="background: ${randomHex}"></div>
-            <span class="hex-value">${randomHex}</span>`;
+        const color = document.createElement("li");
+        color.classList.add("color");
+        color.innerHTML = `<div class="react-box" style="background: ${randomHex}"></div> <span class="hex-value">${randomHex}</span>`;
+
+        container.appendChild(color);
 
 
     }
